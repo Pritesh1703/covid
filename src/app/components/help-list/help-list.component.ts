b79627d496140274fe71b53f529a519d7a39f42c
Gaurav Singh <gaurav.mail125@gmail.com>
2020-04-25 22:56:50 +0530

    added modal feature

--- src/app/components/help-list/help-list.component.ts
+++ src/app/components/help-list/help-list.component.ts
@@ -5,34 +5,45 @@ import { HelpServiceService } from 'src/app/services/help-service.service';
 @Component({
   selector: 'app-help-list',
   templateUrl: './help-list.component.html',
-  styleUrls: ['./help-list.component.scss']
+  styleUrls: ['./help-list.component.scss'],
 })
 export class HelpListComponent implements OnInit {
-
   closeResult = '';
 
   helpList = [];
+  helpObj = {
+    action: 'extend',
+    helpDescription: 'need ',
+    address: 'Hyderabad',
+    mobileNumber: '999999999',
+    email: 'gaurav.mali125@gmail.com',
+    name: 'Gaurav Singh',
+  };
 
-  constructor(private modalService: NgbModal, private helpSvc: HelpServiceService) { }
+  constructor(
+    private modalService: NgbModal,
+    private helpSvc: HelpServiceService
+  ) {}
 
   ngOnInit(): void {
     this.helpList = this.helpSvc.getHelpData();
   }
 
-  open(content) {
-    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
-      this.closeResult = `Closed with: ${result}`;
-    }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-    });
-  }
-
-  openModal() {
-    
+  open(content, data) {
+    this.helpObj = data;
+    this.modalService
+      .open(content, { ariaLabelledBy: 'modal-basic-title' })
+      .result.then(
+        (result) => {
+          this.closeResult = `Closed with: ${result}`;
+        },
+        (reason) => {
+          this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+        }
+      );
   }
 
   private getDismissReason(reason: any) {
     console.log(reason);
   }
-
 }
