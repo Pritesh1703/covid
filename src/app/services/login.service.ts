67cec7fffeb32e4ae5752e9324c3926a83791e53
Gaurav Singh <gaurav.mail125@gmail.com>
2020-04-25 18:09:23 +0530

    login logout

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +9,7 @@ import * as firebase from 'firebase';
 export class LoginService {
   public user: firebase.User;
 
-  constructor(private afAuth: AngularFireAuth) { 
+  constructor(private afAuth: AngularFireAuth, private route: Router) { 
     afAuth.authState.subscribe(user => this.user = user)
   }
 
@@ -18,11 +19,13 @@ export class LoginService {
 
   login() {
     this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider()).finally(() => {
-      
+      this.route.navigate(['dashboard']);
     });
   }
 
   logout() {
-    this.afAuth.auth.signOut();
+    this.afAuth.auth.signOut().finally(() => {
+      this.route.navigate(['login']);
+    });
   }
 }
