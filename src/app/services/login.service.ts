c6999ccc31ec8c17ca8602dbde34e9933a52b0af
Gaurav Singh <gaurav.mail125@gmail.com>
2020-04-25 18:07:35 +0530

    login logout

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
+  public user: firebase.User;
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth) { 
+    afAuth.authState.subscribe(user => this.user = user)
+  }
+
+  public getUser() {
+    return this.afAuth.authState;
+  }
+
+  login() {
+    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider()).finally(() => {
+      
+    });
+  }
+
+  logout() {
+    this.afAuth.auth.signOut();
+  }
 }
