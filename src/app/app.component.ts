71a68d78ce020d55790ad4bc0ca77a28ee417005
Pritesh Kumar <pritesh.kumar1703@gmail.com>
2020-04-26 06:10:09 +0530

    styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ export class AppComponent {
 
   constructor() {
     if(!JSON.parse(localStorage.getItem('help'))) {
+      console.log('pritesh');
       localStorage.setItem('help', JSON.stringify(data.data));
     }
   }
